a7500624bf2ecadcc04f1bc4cbba5dd263d85147
Sharan <89379489+SharanUshurgit@users.noreply.github.com>
2021-09-14 01:53:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
     description = "Name of security group"
-    default     = "my-jenkins-security-group"
+    default     = "my-jenkins-security-group2"
 }
 
 variable "tag_name" {
