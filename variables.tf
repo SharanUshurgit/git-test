e0233f4314173d2dec2dbca0425dcc2abc7299d4
Sharan <89379489+SharanUshurgit@users.noreply.github.com>
2021-09-15 23:43:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,12 +15,12 @@ variable "instance_type" {
 
 variable "security_group" {
     description = "Name of security group"
-    default     = "my-jenkins-security-group2"
+    default     = "my-jenkins-security-group5"
 }
 
 variable "tag_name" {
     description = "Tag Name of for Ec2 instance"
-    default     = "my-ec2-instance"
+    default     = "terraform_ip"
 }
 variable "ami_id" {
     description = "AMI for Ubuntu Ec2 instance"
